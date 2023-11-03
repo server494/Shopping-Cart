5103927571791ee41006a5ca31c1175942134207
Arjun8138 <arjunkunju53@gmail.com>
2023-11-03 11:17:56 +0530

    First commit to TSK-2

--- src/test/java/com/work/ecart/Test1.java
+++ src/test/java/com/work/ecart/Test1.java
@@ -3,6 +3,5 @@ package com.work.ecart;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("TEST 1 ADDED");
-
     }
 }
---
+++ src/test/java/com/work/ecart/Test2.java
@@ -0,0 +1,9 @@
+package com.work.ecart;
+
+public class Test2 {
+    public static void main(String[] args) {
+
+        System.out.println("TEST 2 ADDED FOR TSK-2 BRANCH");
+
+    }
+}
